372ac99b3565a6b3300584bed0fecec9db5aa4f6
nli75 <anneli.halldn@student.kyh.se>
2011-01-11 15:36:34 +0100

    Nu är det tomt..

---
+++ src/se/kyh/ad10s/scrumapp/StartClass.java
@@ -0,0 +1,13 @@
+package se.kyh.ad10s.scrumapp;
+
+public class StartClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
