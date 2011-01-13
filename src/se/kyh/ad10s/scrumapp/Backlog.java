faf0004d7582a8983af46437070b13a02290266f
nli75 <anneli.halldn@student.kyh.se>
2011-01-13 13:18:24 +0100

    Get PbItems Object and add in MyPbItems Arraylist

--- src/se/kyh/ad10s/scrumapp/Backlog.java
+++ src/se/kyh/ad10s/scrumapp/Backlog.java
@@ -1,6 +1,22 @@
 package se.kyh.ad10s.scrumapp;
 
+import java.util.ArrayList;
+
 public class Backlog {
+	
+	//skapa en array som kan ta emot PbItemsobjekt
+	ArrayList<PbItem> MyPbItems = new ArrayList<PbItem>();
+	
+	
+	//method som kan hämta ett PbItemobjekt.
+	// som lägg detta objekt i arrayen. 
+//	För tillfället från StartClass
+	public void savePbItem(PbItem o){
+		MyPbItems.add(o);
+	}
+	
+	
+
 	
 	
 	
