75d8cff452b32ed38896f2b28193321d422ea206
nli75 <anneli.halldn@student.kyh.se>
2011-01-13 14:36:18 +0100

    PbItem can hold Tasks

--- src/se/kyh/ad10s/scrumapp/PbItem.java
+++ src/se/kyh/ad10s/scrumapp/PbItem.java
@@ -1,15 +1,19 @@
 package se.kyh.ad10s.scrumapp;
 
+import java.util.ArrayList;
+
 public class PbItem extends Item{
 	
 	//Knows its backlog - Oh but why?
 	int BacklogId;
 	
+	//I PbItem kan du också hålla Tasks i en array
+	ArrayList<Task> MyTasks = new ArrayList<Task>();
 	
-	
-	public PbItem(){
-//	System.out.println(description);
+	//Tasksobjekt hämtas när ett nytt PbItem skapas i Whiteboard
+	//Lägg in Task-objekt i arrayen MyTasks
+	public void saveTask(Task myTask){
+		MyTasks.add(myTask);
 	}
-	
 
 }
