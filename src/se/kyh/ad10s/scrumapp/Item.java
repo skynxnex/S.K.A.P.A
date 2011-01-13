5aaa4e81ab97421d7d55acfe7c5361d4d013aef0
knorrad <knorrad@gmail.com>
2011-01-13 10:39:26 +0100

    en första item-class
    Signed-off-by: knorrad <knorrad@gmail.com>

---
+++ src/se/kyh/ad10s/scrumapp/Item.java
@@ -0,0 +1,24 @@
+package se.kyh.ad10s.scrumapp;
+
+public class Item {
+	int id;
+	String name;
+	String description;
+	int est;
+	int prio;
+	
+	public Item(){
+		//I en constructor skulle vi kunna skapa en loop
+		
+		//Loopen räknar från id 0++
+		
+		//för varje varv går vi in i våran databas och hämtar ut data med det id't
+		
+		//för varje varv skapar vi ett nytt Item av den.
+		
+		//tills högsta id är nått.
+		
+		//Så här ska det självklart inte fungera, men det skulle gå.
+	}
+
+}
