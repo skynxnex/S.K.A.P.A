6dc045c24b3cf183dc20df097540543b0f3db398
knorrad <knorrad@gmail.com>
2011-02-02 11:57:53 +0100

    changed in gitignore
    Signed-off-by: knorrad <knorrad@gmail.com>

--- src/se/kyh/ad10s/scrumapp/ScrumApp.java
+++ src/se/kyh/ad10s/scrumapp/ScrumApp.java
@@ -1,18 +1,18 @@
 package se.kyh.ad10s.scrumapp;
 
 
-import se.kyh.ad10s.scrumapp.DAOs.PbItemDAO;
-import se.kyh.ad10s.scrumapp.DAOs.SprintDAO;
-import se.kyh.ad10s.scrumapp.DAOs.TaskDAO;
+import se.kyh.ad10s.scrumapp.menu.MenuHolder;
 
 public class ScrumApp {
 
 	public static void main(String[] args) {
-	PbItem pbitem = new PbItem();
-	Task task = new Task();
-	task = TaskDAO.getTaskFromDB(1);
-	pbitem = PbItemDAO.getPbItemFromDB(1);
-	pbitem.checkPbItemDone(task);
+		MenuHolder menuHolder = new MenuHolder();
+		MenuHolder.startMenu();
+//	PbItem pbitem = new PbItem();
+//	Task task = new Task();
+//	task = TaskDAO.getTaskFromDB(1);
+//	pbitem = PbItemDAO.getPbItemFromDB(1);
+//	pbitem.checkPbItemDone(task);
 
 	}
 
