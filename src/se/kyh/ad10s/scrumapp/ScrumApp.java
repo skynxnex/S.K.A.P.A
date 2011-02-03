dd9bd4148b238c4afd8d157889541174b58c202b
knorrad <knorrad@gmail.com>
2011-02-03 12:12:38 +0100

    trying to get a working copy
    Signed-off-by: knorrad <knorrad@gmail.com>

--- src/se/kyh/ad10s/scrumapp/ScrumApp.java
+++ src/se/kyh/ad10s/scrumapp/ScrumApp.java
@@ -1,7 +1,6 @@
 package se.kyh.ad10s.scrumapp;
 
 
-<<<<<<< HEAD
 import se.kyh.ad10s.scrumapp.DAOs.PbItemDAO;
 import se.kyh.ad10s.scrumapp.DAOs.SprintDAO;
 import se.kyh.ad10s.scrumapp.DAOs.TaskDAO;
@@ -9,15 +8,10 @@ import se.kyh.ad10s.scrumapp.concretecommand.ChooseBacklog;
 import se.kyh.ad10s.scrumapp.menu.StartMenu;
 =======
 import se.kyh.ad10s.scrumapp.menu.MenuHolder;
->>>>>>> branch 'refs/heads/master' of https://nli75@github.com/skynxnex/S.K.A.P.A.git
 
 public class ScrumApp {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-	StartMenu menu = new StartMenu();
-	menu.menu();
-=======
 		MenuHolder menuHolder = new MenuHolder();
 		MenuHolder.startMenu();
 //	PbItem pbitem = new PbItem();
@@ -25,7 +19,6 @@ public class ScrumApp {
 //	task = TaskDAO.getTaskFromDB(1);
 //	pbitem = PbItemDAO.getPbItemFromDB(1);
 //	pbitem.checkPbItemDone(task);
->>>>>>> branch 'refs/heads/master' of https://nli75@github.com/skynxnex/S.K.A.P.A.git
 
 	}
 
